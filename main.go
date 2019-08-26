444b0849643c5b2ef43b6229359ae0aeb3fc9eec
Nick Chaloult <npchaloult@gmail.com>
2019-08-26 18:45:23 -0400

    Brought in httprouter; created first test "hello world" endpoint

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
+)
+
+func sayHello(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprintf(w, "Hi everyone!")
+}
 
 func main() {
-	fmt.Println("Hi everyone!")
+	router := httprouter.New()
+
+	router.GET("/sayhello", sayHello)
+
+	fmt.Println("Listening @ localhost:8080....")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
