b6cf7a446e8f47da24d218c39409a2f2566babfb
Nick Chaloult <npchaloult@gmail.com>
2019-08-27 00:43:38 -0400

    Made a pointer to the db connection available to get

--- db/db.go
+++ db/db.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 )
 
+var db *sql.DB
+
 // ConnectToDB attempts to connect to the Postgres database running in a container
 // Spin up with: docker-compose up -d when in root dir
 //
@@ -16,11 +18,20 @@ func ConnectToDB() {
 		panic(err)
 	}
 
+	// Check that our connection to the db seems alright
 	err = connection.Ping()
 	if err != nil {
 		// TODO: real error handling
 		panic(err)
 	}
 
+	// Set global db var equal to the connection pointer
+	db = connection
+
 	fmt.Println("Successfully connected to Postgres")
+}
+
+// GetDB returns a pointer to the database that ConnectToDB() connects to
+func GetDB() *sql.DB {
+	return db
 }
